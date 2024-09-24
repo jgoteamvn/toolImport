ed28dd9b2f62ea0a6413eb78e16f9e936c146363
tuanpinggo <tuannh@pinggo.vn>
2024-09-24 17:36:58 +0700

    fix step 1

--- src/pages/step1.js
+++ src/pages/step1.js
@@ -4,15 +4,29 @@ import { useState } from "react"
 export default function Step1(){
 
     const [loading,setLoading] = useState("")
+    const [data,setData] = useState([])
 
     const handleAction = async (api) => {
+        setData()
         setLoading(`đang tải ${api}`)
         try {
-            const request = await axios.get(`/api/${api}`)
-            console.log("🚀 ~ handleAgency ~ request:", request.data)
+            const request = await axios.get(`/api/new/${api}`)
             setLoading(`Lấy thành công ${api}`)
+            setData(request.data)
         } catch (error) {
             console.log("🚀 ~ handleAgency ~ error:", error)
+            setLoading("lỗi")
+        }
+    }
+
+    const handleWrite = async() => {
+        setData()
+        setLoading(`đang tải`)
+        try {
+            await axios.get(`/api/new/writeJson`)
+            setLoading(`Lấy thành công`)
+        } catch (error) {
+            console.log("🚀 ~ handleWrite ~ error:", error)
             setLoading("lỗi")
         }
     }
@@ -22,8 +36,11 @@ export default function Step1(){
             <div><button onClick={()=>handleAction('getAgency')}>Đồng bộ Agency</button></div>
             <div><button onClick={()=>handleAction('getCategory')}>Đồng bộ category</button></div>
             <div><button onClick={()=>handleAction('getPerson')}>Đồng bộ sign person</button></div>
-
+            <div><button onClick={handleWrite}>Ghi file Jgon</button></div>
             {loading}
+            <div>
+                {data && JSON.stringify(data)}
+            </div>
         </>
     )
 }
