7ab58154a1001a22a1d3cc654cd7752777509307
tuanpinggo <tuannh@pinggo.vn>
2024-09-28 10:17:29 +0700

    fix tools

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,56 +1,98 @@
-import Doc from "@/components/doc";
-import axios from "axios";
-import { useState } from "react";
+import { useState } from "react"
 
-export default function Home() {
-
-  const [datas,setDatas] = useState()
-
-  const [action,setAction] = useState(0)
-
-  const handleClick = async () => {
-    try {
-      const request = await axios.get('/api/getFolder')
-      setDatas(request.data)
-    } catch (error) {
-      console.log("🚀 ~ handleClick ~ error:", error?.response?.data)
-    }
-  }
-
-  const handleStart = () => setAction(1)
-
-  return (
-    <>
-      
-      <div style={{display: 'flex', direction: "row", gap: 10}}>
-        <table>
-          <thead>
-          <tr>
-              <td>id</td>
-              <td>folder name</td>
-              <td>loading</td>
-          </tr>
-          </thead>
-          <tbody>
-            {datas && datas?.map((item,key) => 
-              <Doc 
-                key={key} 
-                stt={key + 1} 
-                id={key + 1} 
-                folderName={item} 
-                action={action} 
-                setAction={setAction}
-                end={datas?.length}
-              />
-            )}
-          </tbody>
-        </table>
-        <div style={{position: 'fixed', top: 10, right: 50}}>
-          <button onClick={handleClick}>click</button>
-          <button onClick={handleStart}>bắt đầu</button>
-          <div>dang xu ly {action} / {datas?.length}</div>
+function LabelStep({step}){
+    return(
+        <div style={styles.titleStep}>
+            <div style={step === 1 ? styles.stepActive : styles.step}>
+                <p>Step 1:</p>
+                <p>Chọn Folder data</p>
+            </div>
+            <div style={step === 2 ? styles.stepActive : styles.step}>
+                <p>Step 2:</p>
+                <p>Đồng bộ Đơn vị</p>
+            </div>
+            <div style={step === 3 ? styles.stepActive : styles.step}>
+                <p>Step 3:</p>
+                <p>Đồng bộ Danh mục văn bản</p>
+            </div>
+            <div style={step === 4 ? styles.stepActive : styles.step}>
+                <p>Step 4:</p>
+                <p>Đồng bộ người ký</p>
+            </div>
+            <div style={step === 5 ? styles.stepActive : styles.step}>
+                <p>Step 5:</p>
+                <p>Đồng bộ dữ liệu</p>
+            </div>
+            <div style={step === 6 ? styles.stepActive : styles.step}>
+                <p>Step 6:</p>
+                <p>Đẩy văn bản lên server</p>
+            </div>
         </div>
-      </div>
-    </>
-  );
+    )
+}
+
+function Step1({folderName,setFolderName,setStep}){
+    return(
+        <div>
+            <p style={{marginBottom:10}}>Step1: Nhập tên folder data</p>
+            <input
+                name="folderName"
+                id="folderName"
+                value={folderName}
+                onChange={(e) => setFolderName(e.target.value)}
+                placeholder="Nhập tên foler"
+                style={{
+                    minWidth: 300
+                }}
+            />
+            <div style={styles.buttonAction}>
+                <buton onClick={() => setStep(2)}>Next Step</buton>
+            </div>
+        </div>
+    )
+}
+
+function Step2({folderName,setStep}){
+
+}
+
+export default function IndexPage(){
+
+    const [folderName,setFolderName] = useState("")
+    const [step,setStep] = useState(1)
+
+    return(
+        <div style={{padding: 30, width: 1000}}>
+            <LabelStep step={step} />
+            <Step1 
+                folderName={folderName} 
+                setFolderName={setFolderName}
+                setStep={setStep}
+            />
+        </div>
+    )
+}
+
+const styles = {
+    titleStep:{
+        display: 'flex',
+        flexDirection: 'row',
+        gap: 30,
+        marginBottom: 30,
+        paddingBottom: 30,
+        borderBottom: '1px solid #eee'
+    },
+    step:{
+        fontWeight: 400
+    },
+    stepActive: {
+        fontWeight: 700, color: "yellow"
+    },
+    buttonAction: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        margin: '30px 0',
+        minWidth: 500
+    }
 }
